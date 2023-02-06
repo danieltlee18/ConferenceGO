4e86d55708ba236ae930b76c1df4052b658008e2
Daniel Lee <danieltlee18@gmail.com>
2023-02-06 18:00:31 -0500

    d1 json views

--- presentations/api_views.py
+++ presentations/api_views.py
@@ -25,15 +25,17 @@ def api_list_presentations(request, conference_id):
         ]
     }
     """
-    presentations = [
-        {
-            "title": p.title,
-            "status": p.status.name,
-            "href": p.get_api_url(),
-        }
-        for p in Presentation.objects.filter(conference=conference_id)
-    ]
-    return JsonResponse({"presentations": presentations})
+    response = []
+    presentations = Presentation.objects.all()
+    for presentation in presentations:
+        response.append(
+            {
+                "title": presentation.title,
+                "status": presentation.status.name,
+                "href": presentation.get_api_url(),
+            }
+        )
+    return JsonResponse({"presentations": response})
 
 
 def api_show_presentation(request, id):
@@ -61,4 +63,19 @@ def api_show_presentation(request, id):
         }
     }
     """
-    return JsonResponse({})
+    presentation = Presentation.objects.get(id=id)
+    return JsonResponse(
+        {
+            "presenter_name": presentation.presenter_name,
+            "company_name": presentation.company_name,
+            "presenter_email": presentation.presenter_email,
+            "title": presentation.title,
+            "synopsis": presentation.synopsis,
+            "created": presentation.created,
+            "status": presentation.status.name,
+            "conference": {
+                "name": presentation.conference.name,
+                "href": presentation.conference.get_api_url(),
+            }
+        }
+    )
